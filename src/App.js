e04c4b77f222192f8c5cc494acacb7744cd1b6c4
Ben Jeavons <bjeavons@users.noreply.github.com>
2020-04-17 17:15:29 -0700

    Clear out CRA and Begin basics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 /* global fetch */
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
@@ -20,22 +19,6 @@ const App = () => {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{message}</p>
-        <p>Change me!</p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
